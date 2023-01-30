1cd49d64743b3119beb3b8930d605bc406213e94
Denis Karmyshakov <denis.karmyshakov@gmail.com>
2023-01-30 23:26:50 +0300

    Bottom sheet navigation

--- app/src/main/kotlin/app/denis55ka/template/MainActivity.kt
+++ app/src/main/kotlin/app/denis55ka/template/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.remember
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -15,6 +19,10 @@ import app.denis55ka.core.ui.viewmodel.applicationViewModels
 import app.denis55ka.main.ui.MainScreen
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.material.BottomSheetNavigator
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,22 +30,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavHostController
 
-    @OptIn(ExperimentalAnimationApi::class)
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class, ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             Themes.Main {
                 navController = rememberAnimatedNavController()
-                AnimatedNavHost(
-                    navController,
-                    startDestination = MainScreen.route,
-                    enterTransition = { ScreenAnimations.EnterSlide },
-                    exitTransition = { ScreenAnimations.ExitSlide },
-                    popEnterTransition = { ScreenAnimations.PopEnterSlide },
-                    popExitTransition = { ScreenAnimations.PopExitSlide },
-                ) {
-                    viewModel.navContributors.forEach { it.contribute(navController) }
+                val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden, skipHalfExpanded = true)
+                val bottomSheetNavigator = remember { BottomSheetNavigator(sheetState) }
+                navController = rememberAnimatedNavController(bottomSheetNavigator)
+                ModalBottomSheetLayout(bottomSheetNavigator) {
+                    AnimatedNavHost(
+                        navController,
+                        startDestination = MainScreen.route,
+                        enterTransition = { ScreenAnimations.EnterSlide },
+                        exitTransition = { ScreenAnimations.ExitSlide },
+                        popEnterTransition = { ScreenAnimations.PopEnterSlide },
+                        popExitTransition = { ScreenAnimations.PopExitSlide },
+                    ) {
+                        viewModel.navContributors.forEach { it.contribute(navController) }
+                    }
                 }
             }
         }
--- feature/feature3/src/main/kotlin/app/denis55ka/feature3/ui/Feature3NavContributor.kt
+++ feature/feature3/src/main/kotlin/app/denis55ka/feature3/ui/Feature3NavContributor.kt
@@ -1,6 +1,5 @@
 package app.denis55ka.feature3.ui
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.navDeepLink
@@ -8,19 +7,20 @@ import app.denis55ka.core.ApplicationComponentScope
 import app.denis55ka.core.navigation.DeeplinkFactory
 import app.denis55ka.core.navigation.NavContributor
 import app.denis55ka.core.navigation.extension.navigateTab
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.bottomSheet
 import app.denis55ka.main.ui.MainScreen
 import app.denis55ka.main.ui.MainTab
-import com.google.accompanist.navigation.animation.composable
 import com.squareup.anvil.annotations.ContributesMultibinding
 import javax.inject.Inject
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(ExperimentalMaterialNavigationApi::class)
 @ContributesMultibinding(ApplicationComponentScope::class)
 class Feature3NavContributor @Inject constructor() : NavContributor {
 
     context(NavGraphBuilder)
     override fun contribute(navController: NavController) {
-        composable(
+        bottomSheet(
             Feature3Screen.route,
             deepLinks = listOf(navDeepLink { uriPattern = DeeplinkFactory.create(Feature3Screen.route) }),
         ) {
